6be51911f33395842f818b77875f06befbdfc392
KryptoCrash <kryptocrashmh@gmail.com>
2020-05-08 02:22:07 +0000

    Drift Legacy -> Drift

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ exports.run = async (bot,message) =>
 {
   console.log(`\n\n${bot.user.username} is at your service.`);
   console.log("\n\nReady to begin! Serving " + bot.guilds.size + " guilds.\n\n");
-  bot.user.setPresence({ status: "online", game: {name: `${bot.users.size.toLocaleString()} users || ${botsettings.prefix}help`}}); 
+  bot.user.setPresence({ status: "online", game: {name: `${botsettings.prefix}help for ${bot.users.size.toLocaleString()} users.`}}); 
 };
